63ceb92a6b19f280a443fb25b694781df9659002
hadson0 <hadsonsb2221@gmail.com>
2022-06-03 18:22:26 -0300

    Created a round screen

---
+++ Client/UI/Screens/roundscreen.cpp
@@ -0,0 +1,28 @@
+#include "roundscreen.h"
+
+RoundScreen::RoundScreen(QString sentence, QString drawingData, QWidget *parent)
+    : Screen{parent} {
+    setPadding(15);
+    setSpacing(10);
+
+    // Next button
+    nextButton = new CustomPushButton("Next", this);
+    connect(nextButton, &QPushButton::clicked, this, &RoundScreen::nextRound);
+
+    // Game sentence frame
+    displayFrame = new RoundFrame(sentence, drawingData, this);
+}
+
+void RoundScreen::resizeEvent(QResizeEvent *event) {
+    Q_UNUSED(event);
+
+    // Next Button
+    int nextButtonWidth = this->getAvaliableWidth() * 0.08, nextButtonHeight = this->getAvaliableHeight() * 0.08;
+    int nextButtonX = this->getAvaliableWidth() - nextButtonWidth, nextButtonY = this->getPadding();
+    nextButton->setGeometry(nextButtonX, nextButtonY, nextButtonWidth, nextButtonHeight);
+
+    // Game sntence frame
+    int displayFrameX = this->getPadding(), displayFrameY = this->getPadding() + nextButtonY + nextButtonHeight;
+    int gameSentenceWidth = this->getAvaliableWidth(), gameSentenceHeight = this->getAvaliableHeight() - nextButtonHeight - this->getSpacing();
+    displayFrame->setGeometry(displayFrameX, displayFrameY, gameSentenceWidth, gameSentenceHeight);
+}
---
+++ Client/UI/Screens/roundscreen.h
@@ -0,0 +1,26 @@
+#ifndef ROUNDSCREEN_H
+#define ROUNDSCREEN_H
+
+#include "UI/Screens/screen.h"
+#include "UI/Components/roundframe.h"
+#include "UI/Components/custompushbutton.h"
+
+class RoundScreen : public Screen {
+    Q_OBJECT
+
+    // Widgets
+    CustomPushButton *nextButton;
+
+    // Frame
+    RoundFrame *displayFrame;
+
+    void resizeEvent(QResizeEvent *event);
+
+public:
+    explicit RoundScreen(QString sentence, QString drawingData, QWidget *parent = nullptr);
+
+signals:
+    void nextRound();
+};
+
+#endif // ROUNDSCREEN_H
