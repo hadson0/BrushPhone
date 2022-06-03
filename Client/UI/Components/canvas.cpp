4020abe915bd9444245a55736e1e990d3846c6b3
hadson0 <hadsonsb2221@gmail.com>
2022-06-03 07:34:19 -0300

    Created a canvas widget

---
+++ Client/UI/Components/canvas.cpp
@@ -0,0 +1,98 @@
+#include "canvas.h"
+
+Canvas::Canvas(QWidget *parent)
+    : QWidget{parent}, penColor(Qt::black), backgroundColor(Qt::white), isDrawing(false), penWidth(5) {
+    startPoint = endPoint = QPoint();
+    // Image
+    image = QImage(this->size(),  QImage::Format_ARGB32);
+}
+
+void Canvas::paintEvent(QPaintEvent *event) {
+    QPainter painter(this);
+    painter.fillRect(image.rect(), backgroundColor); // Otherwise the background would be transparent
+    painter.drawImage(0, 0, image);
+    event->accept();
+}
+
+void Canvas::resizeEvent(QResizeEvent *event) {
+    image = QImage(this->size(),  QImage::Format_ARGB32);
+    clearImage();
+    event->accept();
+}
+
+void Canvas::mousePressEvent(QMouseEvent *event) {
+    if (event->button() == Qt::LeftButton) {
+        startPoint = event->pos();
+        endPoint = event->pos();
+        isDrawing = true;
+    }
+}
+
+void Canvas::mouseReleaseEvent(QMouseEvent *event) {
+    if ((event->buttons() & Qt::LeftButton) && isDrawing) {
+        endPoint = event->pos();
+        this->draw();
+        isDrawing = false;
+    }
+}
+
+void Canvas::mouseMoveEvent(QMouseEvent *event) {
+    if ((event->buttons() & Qt::LeftButton) && isDrawing) {
+        endPoint = event->pos();
+        this->draw();
+    }
+}
+
+void Canvas::draw() {
+    // Painter setup
+    QPainter pantier(&image);
+    QPen pen(penColor, penWidth, Qt:: SolidLine, Qt::RoundCap, Qt::RoundJoin);
+    pantier.setPen(pen);
+
+    // Draws a line from the start point to the endpoint
+    pantier.drawLine(startPoint, endPoint);
+    startPoint = endPoint;
+
+    this->update();
+}
+
+void Canvas::updateImage(QImage *image, const QSize &newSize) {
+    if (image->size() != newSize) {
+        QPainter pantier(image);
+        QImage newImage(newSize, QImage::Format_ARGB32_Premultiplied);
+        newImage.fill(backgroundColor); // Otherwise the background would be transparent
+
+        pantier.drawImage(QPoint(0, 0), *image);
+        *image = newImage;
+    }
+}
+
+void Canvas::setPenColor(QColor &color) { penColor = color; }
+
+void Canvas::setPenWidth(int width) { penWidth = width; }
+
+QColor Canvas::getPenColor() const { return penColor; }
+
+int Canvas::getPenWidth() const { return penWidth; }
+
+void Canvas::clearImage() {
+    image.fill(backgroundColor);
+    update();
+}
+
+void Canvas::saveImage() {
+    QImage drawing = image;
+    QTemporaryFile file("tempXXXXXX.png");
+    if (file.open()) {
+        drawing.save(file.fileName());
+
+        QByteArray drawingData = file.readAll();
+        file.write(drawingData);
+
+        updateImage(&drawing, size());
+
+        emit sendDrawing(drawingData.toHex());
+    } else {
+        qDebug() << "Error";
+    }
+}
---
+++ Client/UI/Components/canvas.h
@@ -0,0 +1,52 @@
+#ifndef CANVAS_H
+#define CANVAS_H
+
+#include <QWidget>
+#include <QColor>
+#include <QImage>
+#include <QPoint>
+#include <QMouseEvent>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QPainterPath>
+#include <QTemporaryFile>
+#include <QDebug>
+
+class Canvas : public QWidget {
+    Q_OBJECT
+
+    QColor penColor, backgroundColor;
+    bool isDrawing;
+    int penWidth;
+    QPoint startPoint, endPoint;
+    QImage image;
+
+    void paintEvent(QPaintEvent *event) ;
+    void resizeEvent(QResizeEvent *event);
+
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+    void draw();
+    void updateImage(QImage *image, const QSize &newSize);
+
+public:
+    explicit Canvas(QWidget *parent = nullptr);
+
+    void setPenColor(QColor &color);
+    void setPenWidth(int newWidth);
+
+    QColor getPenColor() const;
+    int getPenWidth() const;
+
+    void saveImage();
+
+public slots:
+    void clearImage();
+
+signals:
+    void sendDrawing(QString drawingData);
+};
+
+#endif // CANVAS_H
